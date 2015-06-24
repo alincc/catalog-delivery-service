7bd73260a390fe39332df85df9fa23c0e088d2aa
Andreas Bjørnådal <bjornadal@gmail.com>
2015-06-24 13:11:15 +0200

    Initial commit

---
+++ catalog-delivery-service-rest/src/main/java/no/nb/microservices/delivery/config/ApplicationSettings.java
@@ -0,0 +1,10 @@
+package no.nb.microservices.delivery.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "microservice")
+public class ApplicationSettings {
+
+}
---
+++ catalog-delivery-service-rest/src/main/java/no/nb/microservices/delivery/rest/controller/HomeController.java
@@ -0,0 +1,60 @@
+package no.nb.microservices.delivery.rest.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@Api(value = "/", description = "Home api")
+public class HomeController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HomeController.class);
+
+    @ApiOperation(value = "Hello World", notes = "Hello World notes", response = String.class)
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Successful response") })
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        return "Hello world";
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "It looks like we have a internal error in our application. The error have been logged and will be looked at by our development team.")
+    public void defaultHandler(HttpServletRequest req, Exception e) {
+
+        // Build Header string
+        StringBuilder headers = new StringBuilder();
+        for (String headerKey : Collections.list(req.getHeaderNames())) {
+            String headerValue = req.getHeader(headerKey);
+            headers.append(headerKey + ": " + headerValue + ", ");
+        }
+
+        LOG.error("" +
+                "Got an unexcepted exception.\n" +
+                "Context Path: " + req.getContextPath() + "\n" +
+                "Request URI: " + req.getRequestURI() + "\n" +
+                "Query String: " + req.getQueryString() + "\n" +
+                "Method: " + req.getMethod() + "\n" +
+                "Headers: " + headers + "\n" +
+                "Auth Type: " + req.getAuthType() + "\n" +
+                "Remote User: " + req.getRemoteUser() + "\n" +
+                "Username: " + ((req.getUserPrincipal()  != null) ? req.getUserPrincipal().getName() : "Anonymous") + "\n"
+                , e);
+    }
+}
