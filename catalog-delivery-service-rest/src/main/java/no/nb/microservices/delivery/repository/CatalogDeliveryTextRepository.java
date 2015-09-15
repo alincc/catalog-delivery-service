8b83b37a9fea3a6b035ff04dd2df866e49313b5f
Andreas Bjørnådal <bjornadal@gmail.com>
2015-09-15 13:06:13 +0200

    Added missing values for RequestParams

--- catalog-delivery-service-rest/src/main/java/no/nb/microservices/delivery/repository/CatalogDeliveryTextRepository.java
+++ catalog-delivery-service-rest/src/main/java/no/nb/microservices/delivery/repository/CatalogDeliveryTextRepository.java
@@ -2,6 +2,7 @@ package no.nb.microservices.delivery.repository;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,9 +14,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient("catalog-delivery-text-service")
 public interface CatalogDeliveryTextRepository {
 
-    @RequestMapping(value = "/alto/{urn}", method = RequestMethod.GET)
-    ByteArrayResource getAltos(@PathVariable String urn, @RequestParam String pages, @RequestParam String pageSelection);
+    @RequestMapping(value = "/alto/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    ByteArrayResource getAltos(@PathVariable("id") String id, @RequestParam("pages") String pages, @RequestParam("pageSelection") String pageSelection);
 
     @RequestMapping(value = "/text/{urn}", method = RequestMethod.GET)
-    ByteArrayResource getText(@PathVariable String urn, @RequestParam String pages, @RequestParam String pageSelection);
+    ByteArrayResource getText(@PathVariable("urn") String urn, @RequestParam("pages") String pages, @RequestParam("pageSelection") String pageSelection);
 }
